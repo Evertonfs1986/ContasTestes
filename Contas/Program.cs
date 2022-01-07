2f59e96ba3ab7c9ef3d4aa36fd30deb578d76642
Evertonfs1986 <evertonfs.1986@gmail.com>
2022-01-07 12:21:13 -0300

    Adicionar arquivos de projeto.

---
+++ Contas/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    public class Contas
+{
+    public  int Soma(int a, int b)
+    {
+        return a + b;
+    }
+    public int Subtracao(int a, int b)
+    {
+        return a - b;
+    }
+    public int Divisao(int a, int b)
+    {
+        return a / b;
+    }
+    public int Multiplicacao(int a, int b)
+    {
+        return a * b;
+    }
+}   
